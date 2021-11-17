013db8cb1fa67bbcd57c74d4bcc308cd9361b3fa
Otchere-Peprah Kwaku <88670877+PlumioHarvey@users.noreply.github.com>
2021-11-17 15:14:16 +0000

    Add files via upload

--- App.js
+++ App.js
@@ -1,7 +1,37 @@
+
+import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {View} from 'react-native';
-import WelcomeScreen from './app/screens/WelcomeScreen';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import loginScreen from './screens/loginScreen';
+import homeScreen from './screens/homeScreen';
+
+import cart from './screens/cart';
+import productDetails from './screens/productDetails';
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
-  return <WelcomeScreen />;
-}  
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen options= {{headerShown: false}} name="Log in" component={loginScreen} />
+        <Stack.Screen options= {{headerShown: false}} name="Home" component={homeScreen} />
+        
+        <Stack.Screen options= {{headerShown: false}} name="cart" component={cart} />
+        <Stack.Screen options= {{headerShown: false}} name="Details" component={productDetails} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const StyleSheet = StyleSheet.create({
+  container: { 
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  
+});
